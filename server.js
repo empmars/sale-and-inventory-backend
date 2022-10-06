c7b0b9aef5bfa2102a7db954665ff54b850b1fbd
empmars <99082987+empmars@users.noreply.github.com>
2022-10-07 04:13:08 +0500

    Table Add

--- server.js
+++ server.js
@@ -23,6 +23,7 @@ app.get('/', function (req, res) {
 })
 
 app.post('/add-item', function (req, res) {
+  console.log(req.body)
           const { name , quantity , price , expiry , profit } = req.body;
 
           const valuesOfReq = Object.values(req.body);
@@ -86,6 +87,72 @@ app.put('/del-item', function(req,res) {
       .catch(err=>res.json(err.detail))
 
 })
+
+app.post('/sale-item-search', function(req,res) {
+    var { name } = req.body
+    name = name.toString().toLowerCase();
+    knex('items')
+    .select('name')
+    .then(result=>{
+     
+      const matched = result.filter((item)=>{
+        return (item.name.toLowerCase().includes(name))
+      })
+
+      res.json(matched)
+    })
+})
+
+app.post('/sale-item-add', function(req,res) {
+
+      knex.transaction(function(trx) {
+            var { name , quantity , discount , price } = req.body
+
+            knex('items')
+            .where({name: name})
+            .select('price')
+            .then(result1=>{
+                 var singlePrice = result1[0].price;
+                 price = singlePrice * quantity
+            
+                 knex('sale')
+                  .insert({
+                    name: name,
+                    quantity: quantity,
+                    price: price,
+                    discount: discount,
+                    date: new Date()
+
+                  })
+                  .returning('*')
+                  .then(result2=>{
+                    res.json(result2)
+                  })
+
+            })
+            .then(trx.commit)
+            .catch(trx.rollback);
+
+          })
+          .catch(err=>console.log(err))
+
+
+
+    // knex('sale')
+    // .insert({
+    //   name: name,
+    //   quantity: quantity,
+    //   date: new Date()
+    // })
+    // .returning('*')
+    // .then(result=>{
+    //   console.log(result)
+    // })
+
+})
+
+
+
 
 
 app.listen(3001)
