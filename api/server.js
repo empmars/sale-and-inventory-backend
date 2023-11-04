f4312c3c91f98c97732793a409e311e47f7d0f94
empmars <mssami6541.3@gmail.com>
2023-11-05 02:04:39 +0500

    as\

--- api/server.js
+++ api/server.js
@@ -32,46 +32,46 @@ app.get('/', function (req, res) {
 
 
 app.post('/add-item', function (req, res) {
-          const { name , quantity , price , expiry , profit } = req.body;
+          
 
-          console.log(req.body)
+          console.log('req.body')
 
-          for (let par in req.body) {
-              if(req.body[par].length === 0 && par !== 'expiry') {
-                req.body[par] = '0'
-              }
-          }
+          // for (let par in req.body) {
+          //     if(req.body[par].length === 0 && par !== 'expiry') {
+          //       req.body[par] = '0'
+          //     }
+          // }
 
-          const profitPerc= (profit/100) * price;
-          if(expiry.length === 0) {
-                    knex('items')
-                    .insert({
-                      name: name,
-                      quantity : Number(quantity),
-                      price: Number(price),
-                      expiry: null,
-                      profit: Number(profitPerc)
-                    })
-                    .then(result=>{
-                        res.json('success')
-                    })
-                    .catch(err=>res.json(err.detail))
-              } else {
-                knex('items')
-                .insert({
-                  name: name,
-                  quantity : Number(quantity),
-                  price: Number(price),
-                  expiry: expiry,
-                  profit: Number(profitPerc)
-                })
-                .then(result=>{
-                    res.json('success')
-                })
-                .catch(err=>res.json(err.detail))
+          // const profitPerc= (profit/100) * price;
+          // if(expiry.length === 0) {
+          //           knex('items')
+          //           .insert({
+          //             name: name,
+          //             quantity : Number(quantity),
+          //             price: Number(price),
+          //             expiry: null,
+          //             profit: Number(profitPerc)
+          //           })
+          //           .then(result=>{
+          //               res.json('success')
+          //           })
+          //           .catch(err=>res.json(err.detail))
+          //     } else {
+          //       knex('items')
+          //       .insert({
+          //         name: name,
+          //         quantity : Number(quantity),
+          //         price: Number(price),
+          //         expiry: expiry,
+          //         profit: Number(profitPerc)
+          //       })
+          //       .then(result=>{
+          //           res.json('success')
+          //       })
+          //       .catch(err=>res.json(err.detail))
 
 
-              }
+          //     }
 })
 
 app.post('/list-search-edit', function(req,res) {
