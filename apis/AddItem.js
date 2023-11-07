7fe980e3b3426be438a319942de14cd88a70a8ae
empmars <mssami6541.3@gmail.com>
2023-11-07 22:14:40 +0500

    as

--- apis/AddItem.js
+++ apis/AddItem.js
@@ -7,68 +7,69 @@ async function AddItem(req, res, headers) {
 
     const profitPerc = (profit / 100) * price;
     quantity = Number(quantity)
-   if (expiry.length === 0) {
+    console.log(name, price, profit, quantity, expiry)
+//    if (expiry.length === 0) {
 
-        try {
+//         try {
 
-            await client.sql`INSERT INTO items (name , quantity , price , profit , expiry) VALUES (${name} , ${quantity} , ${price} , ${profit} , ${null})`
-            res.json('success')
-
-
-        } catch (err) {
-            res.json(err.detail)
-        }
+//             await client.sql`INSERT INTO items (name , quantity , price , profit , expiry) VALUES (${name} , ${quantity} , ${price} , ${profit} , ${null})`
+//             res.json('success')
 
 
-
-
-        //     knex('items')
-        //       .insert({
-        //         name: name,
-        //         quantity: Number(quantity),
-        //         price: Number(price),
-        //         expiry: null,
-
-
-        //         profit: Number(profitPerc)
-        //       })
-        //       .then(result => {
-        //         res.json('success')
-        //       })
-        //       .catch(err => res.json(err.detail))
-    } else {
-
-        try {
-
-            await client.sql`INSERT INTO items (name , quantity , price , profit , expiry) VALUES (${name} , ${quantity} , ${price} , ${profit} , ${expiry})`
-            res.json('success')
-
-
-        } catch (err) {
-            res.json(err.detail)
-        }
+//         } catch (err) {
+//             res.json(err.detail)
+//         }
 
 
 
 
+//         //     knex('items')
+//         //       .insert({
+//         //         name: name,
+//         //         quantity: Number(quantity),
+//         //         price: Number(price),
+//         //         expiry: null,
+
+
+//         //         profit: Number(profitPerc)
+//         //       })
+//         //       .then(result => {
+//         //         res.json('success')
+//         //       })
+//         //       .catch(err => res.json(err.detail))
+//     } else {
+
+//         try {
+
+//             await client.sql`INSERT INTO items (name , quantity , price , profit , expiry) VALUES (${name} , ${quantity} , ${price} , ${profit} , ${expiry})`
+//             res.json('success')
+
+
+//         } catch (err) {
+//             res.json(err.detail)
+//         }
 
 
 
-        //     knex('items')
-        //       .insert({
-        //         name: name,
-        //         quantity: Number(quantity),
-        //         price: Number(price),
-        //         expiry: expiry,
-        //         profit: Number(profitPerc)
-        //       })
-        //       .then(result => {
-        //         res.json('success')
-        //       })
-        //       .catch(err => res.json(err.detail))
 
 
-    }
+
+
+//         //     knex('items')
+//         //       .insert({
+//         //         name: name,
+//         //         quantity: Number(quantity),
+//         //         price: Number(price),
+//         //         expiry: expiry,
+//         //         profit: Number(profitPerc)
+//         //       })
+//         //       .then(result => {
+//         //         res.json('success')
+//         //       })
+//         //       .catch(err => res.json(err.detail))
+
+
+//     }
 
 
 }
